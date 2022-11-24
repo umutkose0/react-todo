684cd2a48d53cf58d91b4d77c9b1b29cc100e1ff
Umut Köse <umut2819@gmail.com>
2022-11-25 00:27:56 +0300

    Store data with localStorage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import './App.css';
 import Header from "./components/Header/Header.jsx"
 import ReactModal from "react-modal";
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import * as Icon from 'react-feather';
 function App() {
   ReactModal.setAppElement('#root');
@@ -10,13 +10,21 @@ function App() {
   const [todos,setTodos]=useState([]);
 
   const addTodo=(title,desc)=>{
-    setTodos([...todos,{title,desc}])
+    setTodos([...todos,{title,desc,completed:false}])
     console.log(title,desc,todos);
   }
   const deleteHandle=(index)=>{
     var deletedTodos=todos.filter((todo,i)=>{return i!=index? todo:""})
     setTodos(deletedTodos);
   }
+  useEffect(()=>{
+    if (localStorage.getItem("todos"))
+       setTodos(JSON.parse(localStorage.getItem("todos")))
+  },[])
+  useEffect(()=>{
+    if(todos!="")
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
   return (
     <div className="App">
       <Header 
@@ -28,9 +36,21 @@ function App() {
         {todos.map((todo,i)=>{
           
           return <li key={i}>
-            <span><Icon.Check /></span>{todo.title}
-            <br/><small>{todo.desc}</small>
-                <span onClick={()=>{deleteHandle(i)}}><Icon.X /></span></li>
+              <div>
+                <span>
+                  {todo.completed? <Icon.Check /> :<Icon.Calendar />}
+                </span>
+              </div>
+              <div>
+                {todo.title}
+                <br/><small>{todo.desc}</small>
+              </div>
+              <div>
+                <span onClick={()=>{deleteHandle(i)}}>
+                    <Icon.X />
+                </span>
+              </div>
+            </li>
         })}
       </ul>
       </div>
